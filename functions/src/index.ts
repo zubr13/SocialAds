fed4292659f0d184c52c710565e26214c4d65840
oleksmir <olexiy.miroshnik@gmail.com>
2018-02-03 23:28:39 +0200

    Fix create user function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,15 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
+export interface Roles {
+  moderator: boolean;
+}
+
+export interface AppUser {
+  balance: string | number;
+  roles: Roles;
+}
+
 admin.initializeApp(functions.config().firebase);
 
 const database = admin.database();
@@ -10,5 +19,8 @@ exports.createUser = functions.auth.user().onCreate(event => {
 
   return database.ref(`/users/${user.uid}`).set({
     balance: '0',
-  });
+    roles: {
+      moderator: false,
+    }
+  } as AppUser);
 });
