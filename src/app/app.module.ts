f9ccdf0a4a90b923d9b9c6b037b32c98e346fc40
oleksmir <olexiy.miroshnik@gmail.com>
2018-02-03 18:02:02 +0200

    Add material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButton, MatButtonModule} from '@angular/material';
 
+const M = [MatButtonModule];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    ...M,
     AppRoutingModule
   ],
   providers: [],
